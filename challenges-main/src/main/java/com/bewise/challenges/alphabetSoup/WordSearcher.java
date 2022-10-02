ffe6a79169bf576d18730762d616406b5e7f1222
Germán Nicolás Villalba Nieva <germanicovilla@hotmail.com>
2022-10-02 13:46:21 -0300

    first commit

---
+++ challenges-main/src/main/java/com/bewise/challenges/alphabetSoup/WordSearcher.java
@@ -0,0 +1,14 @@
+package com.bewise.challenges.alphabetSoup;
+
+public class WordSearcher {
+
+    private char soup[][];
+
+    public WordSearcher(char soup[][]) {
+        this.soup = soup;
+    }
+
+    public boolean isPresent(String word) {
+        return false;
+    }
+}
---
+++ challenges-main/src/main/java/com/bewise/challenges/sorting/Sorting.java
@@ -0,0 +1,28 @@
+package com.bewise.challenges.sorting;
+
+import java.util.List;
+
+public class Sorting {
+
+    /**
+     * Se debe ordenar primero por puntuación de manera descendente, luego por nombre de manera ascendente.
+     *
+     * @param jugadores la lista de jugadores a ordenar
+     * @return la lista ordenada de jugadores
+     */
+    public static List<Jugador> ordenarPorPuntuacionYNombre(List<Jugador> jugadores) {
+        return null;
+    }
+
+    /**
+     * Se debe ordenar primero por puntuación de manera descendente. Cuando 2 jugadores tienen igual cantidad de puntos,
+     * el que tiene menos perdidas se lo considerara el mayor. Luego a igual puntos y perdidas se segxxuirá usando el
+     * nombre de manera ascendente.
+     *
+     * @param jugadores la lista de jugadores a ordenar
+     * @return la lista ordenada de jugadores
+     */
+    public static List<Jugador> ordenarPorPuntuacionPerdidasYNombre(List<Jugador> jugadores) {
+        return null;
+    }
+}
